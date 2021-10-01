a9269afced71738704b59a078ee09e48babf422e
EdderEscarrega <edder.peralta@uabc.edu.mx>
2021-10-01 13:47:49 -0600

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,22 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: 'Iphone 13',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description:
+      'La pantalla del iPhone 13 tiene esquinas redondeadas que siguen el elegante diseño curvo del teléfono, y las esquinas se encuentran dentro de un rectángulo estándar. Si se mide en forma de rectángulo estándar, la pantalla tiene 6.06 pulgadas en diagonal (el área real de visualización es menor).',
   },
   {
-    name: 'Phone Mini',
+    name: 'Iphone 13 mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description:
+      'La pantalla del iPhone 13 mini tiene esquinas redondeadas que siguen el elegante diseño curvo del teléfono, y las esquinas se encuentran dentro de un rectángulo estándar. Si se mide en forma de rectángulo estándar, la pantalla tiene 5.42 pulgadas en diagonal (el área real de visualización es menor).',
   },
   {
-    name: 'Phone Standard',
+    name: 'Iphone SE',
     price: 299,
-    description: ''
-  }
+    description: 'La pantalla del iPhone SE Pro tiene esquinas redondeadas que siguen el elegante diseño curvo del teléfono, y las esquinas se encuentran dentro de un rectángulo estándar. Si se mide en forma de rectángulo estándar, la pantalla tiene 6.06 pulgadas en diagonal (el área real de visualización es menor).',
+  },
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
