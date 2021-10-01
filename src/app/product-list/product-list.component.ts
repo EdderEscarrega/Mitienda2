18c806570712e266d8419f081b53d5d2a44af450
EdderEscarrega <edder.peralta@uabc.edu.mx>
2021-10-01 14:18:24 -0600

    4 files modified

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -8,10 +8,15 @@ import { products } from '../products';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent {
+
   products = products;
 
   share() {
-    window.alert('The product has been shared!');
+    window.alert('El producto ha sido compartido!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
   }
 }
 
